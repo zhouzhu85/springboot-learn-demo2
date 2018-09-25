0d5b3a685fe4b139c99e98a70e16a401405999ec
zhouzhu85 <734837014@qq.com>
2018-09-25 18:10:54 +0800

    添加swagger2

---
+++ springboot-restfulapi-test/src/main/java/com/github/zhouzhu/springboot/Swagger2.java
@@ -0,0 +1,41 @@
+package com.github.zhouzhu.springboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @ClassName: Swagger2
+ * @author:zhouzhu
+ * @Date: 2018/9/25 17:26
+ * @Version: V1.0
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    @Bean
+    public Docket createResTApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(""))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact("zhouzhu")
+                .version("1.0")
+                .build();
+    }
+}
--- springboot-restfulapi-test/src/main/java/com/github/zhouzhu/springboot/controller/UserController.java
+++ springboot-restfulapi-test/src/main/java/com/github/zhouzhu/springboot/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.github.zhouzhu.springboot.controller;
 
 import com.github.zhouzhu.springboot.entity.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -17,6 +20,7 @@ public class UserController {
      */
     static Map<Long,User> users= Collections.synchronizedMap(new HashMap<Long,User>());
 
+    @ApiOperation(value = "获取用户列表",notes = "")
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public List<User> getUserList(){
 
@@ -25,13 +29,27 @@ public class UserController {
         return r;
     }
 
+    @ApiOperation(value = "创建用户",notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user",value = "用户详情实体user",required = true,dataType = "user")
     @RequestMapping(value = "/",method = RequestMethod.POST)
     public String postUser(@ModelAttribute User user){
         users.put(user.getId(),user);
         return "success";
     }
 
-    @RequestMapping(value = "{id}",method = RequestMethod.PUT)
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
+
+    @ApiOperation(value = "更新用户详情信息",notes = "根据url的id来指定更新对象，并根据传过来的user信息来更新用户详情信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id",value = "用户ID",required = true,dataType = "Long"),
+            @ApiImplicitParam(name = "user",value = "用户详情实体user",required = true,dataType = "User")
+    })
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
     public String putUser(@PathVariable Long id,@ModelAttribute User user){
         User u=users.get(id);
         u.setName(user.getName());
@@ -40,7 +58,9 @@ public class UserController {
         return "success";
     }
 
-    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
+    @ApiOperation(value = "删除用户",notes = "根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id",value = "用户ID",required = true,dataType = "Long")
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
     public String deleteUser(@PathVariable long id){
         users.remove(id);
         return "success";
