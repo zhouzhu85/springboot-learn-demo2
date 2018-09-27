36560a96ff7d095df539d543dbe93fe840f8a063
zhouzhu85 <734837014@qq.com>
2018-09-27 18:07:25 +0800

    springboot spring-data-jpa

---
+++ springboot-spring-data-jpa/src/main/java/com/github/zhouzhu/SpringbootSpringDataJpaApplication.java
@@ -0,0 +1,12 @@
+package com.github.zhouzhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringDataJpaApplication.class, args);
+	}
+}
---
+++ springboot-spring-data-jpa/src/main/java/com/github/zhouzhu/domain/UserRepository.java
@@ -0,0 +1,16 @@
+package com.github.zhouzhu.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * @author zhouzhu
+ */
+public interface UserRepository extends JpaRepository<User,Long>{
+    User findByName(String name);
+    User findByNameAndAge(String name,Integer age);
+
+//    @Query("select u from user u where u.name=:name")
+//    User findUser(@Param("name") String name);
+}
